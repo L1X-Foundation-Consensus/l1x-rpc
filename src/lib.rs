05f972d29ce040efe992dcb5d7412b14695f51de
Konrad Sosnowski <sosnowski.konrad@gmail.com>
2023-09-13 14:20:50 +1000

    adding conversions to system TransactionType

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,99 @@ use sha3::{Digest, Keccak256};
 
 pub mod rpc_model {
     tonic::include_proto!("rpc_model");
+    use anyhow::anyhow;
+
+    impl TryFrom<submit_transaction_request::TransactionType> for super::transaction::TransactionType {
+        type Error = anyhow::Error;
+
+        fn try_from(
+            value: submit_transaction_request::TransactionType,
+        ) -> Result<Self, Self::Error> {
+            let result_txn_type = match value {
+                submit_transaction_request::TransactionType::NativeTokenTransfer(
+                    NativeTokenTransfer { address, amount },
+                ) => super::transaction::TransactionType::NativeTokenTransfer(
+                    address
+                        .try_into()
+                        .map_err(|_| anyhow!("Failed to convert address bytes"))?,
+                    amount.into(),
+                ),
+                submit_transaction_request::TransactionType::SmartContractDeployment(
+                    SmartContractDeployment {
+                        access_type,
+                        contract_type,
+                        contract_code,
+                    },
+                ) => super::transaction::TransactionType::SmartContractDeployment {
+                    access_type: access_type.try_into()?,
+                    contract_type: contract_type.try_into()?,
+                    contract_code,
+                },
+                submit_transaction_request::TransactionType::SmartContractInit(
+                    SmartContractInit { address, arguments },
+                ) => super::transaction::TransactionType::SmartContractInit(
+                    address
+                        .try_into()
+                        .map_err(|_| anyhow!("Failed to convert address bytes"))?,
+                    arguments,
+                ),
+                submit_transaction_request::TransactionType::SmartContractFunctionCall(
+                    SmartContractFunctionCall {
+                        contract_address,
+                        function_name,
+                        arguments,
+                    },
+                ) => super::transaction::TransactionType::SmartContractFunctionCall {
+                    contract_instance_address: contract_address
+                        .try_into()
+                        .map_err(|_| anyhow!("Failed to convert contract_address bytes"))?,
+                    function: function_name,
+                    arguments,
+                },
+                submit_transaction_request::TransactionType::CreateStakingPool(
+                    CreateStakingPool {
+                        contract_instance_address,
+                        min_stake,
+                        max_stake,
+                        min_pool_balance,
+                        max_pool_balance,
+                        staking_period,
+                    },
+                ) => super::transaction::TransactionType::CreateStakingPool {
+                    contract_instance_address: match contract_instance_address {
+                        Some(x) => Some(x.try_into().map_err(|_| {
+                            anyhow!("Failed to convert contract_instance_address bytes")
+                        })?),
+                        None => None,
+                    },
+                    min_stake: min_stake.map(|x| x.into()),
+                    max_stake: max_stake.map(|x| x.into()),
+                    min_pool_balance: min_pool_balance.map(|x| x.into()),
+                    max_pool_balance: max_pool_balance.map(|x| x.into()),
+                    staking_period: staking_period.map(|x| x.into()),
+                },
+                submit_transaction_request::TransactionType::Stake(Stake {
+                    pool_address,
+                    amount,
+                }) => super::transaction::TransactionType::Stake {
+                    pool_address: pool_address
+                        .try_into()
+                        .map_err(|_| anyhow!("Failed to convert pool_address bytes"))?,
+                    amount: amount.into(),
+                },
+                submit_transaction_request::TransactionType::Unstake(UnStake {
+                    pool_address,
+                    amount,
+                }) => super::transaction::TransactionType::UnStake {
+                    pool_address: pool_address
+                        .try_into()
+                        .map_err(|_| anyhow!("Failed to convert pool_address bytes"))?,
+                    amount: amount.into(),
+                },
+            };
+            Ok(result_txn_type)
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -24,10 +117,11 @@ pub struct TXSignPayload {
 
 pub fn sign(
     secret_key: SecretKey,
-    transaction_type: TransactionType,
+    transaction_type: rpc_model::submit_transaction_request::TransactionType,
     fee_limit: Balance,
     nonce: Nonce,
 ) -> Result<Vec<u8>> {
+    let transaction_type: TransactionType = transaction_type.try_into()?;
     let sign_payload = TXSignPayload {
         nonce,
         transaction_type,
